c4917439fc09d6256192d9cecee9b1725a42dad2
Keon Arbabi <keon.arbabi@gmail.com>
2026-03-29 00:11:31 -0400

    udpate

--- test_basic_rapids_performance.py
+++ test_basic_rapids_performance.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
     print(f'{DATA_PATH=}')
 
     cluster = LocalCUDACluster(
-        CUDA_VISIBLE_DEVICES='0,1,2,3',
+        CUDA_VISIBLE_DEVICES='0,1,2,3,4,5,6,7',
         threads_per_worker=10,
         protocol='ucx',
         rmm_pool_size='10GB',
